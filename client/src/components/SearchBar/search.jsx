c5f9933906e3a2ac65bb103042b9d51ce352b344
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-07-17 14:01:13 +0530

    api/movie end point created on server. client updated (basic)

--- client/src/components/SearchBar/search.jsx
+++ client/src/components/SearchBar/search.jsx
@@ -2,82 +2,73 @@ import React, { useEffect, useState } from "react";
 import "./Search.css";
 import { SearchInput } from "./searchInput/SearchInput";
 import { SearchList } from "./searchList/SearchList";
-import axios from 'axios';
+import axios from "axios";
 
 // method 1: local filteration
-// method 2: Filter through API 
+// method 2: Filter through API
 
-const API_URL =
-    "https://api.themoviedb.org/3/search/movie?api_key=d3449ff6ec0c027623bf6b6f5fff78b3&language=en-US&page=1&include_adult=false";
+const API_URL = "http://localhost:3500/api/movie";
 
 export const Search = () => {
-    //              --------state------------
-    const [searchInputValue, setSearchInputvalue] = useState("");
-    const [searchListValue, setSearchListValue] = useState([]);
+	//              --------state------------
+	const [searchInputValue, setSearchInputvalue] = useState("");
+	const [searchListValue, setSearchListValue] = useState([]);
 
-    //              ---------API Call--------
-    const fetchSearchList = async () => {
-        try {
-            // const response= await axios(API_URL+'&query'+searchInputValue); //old method
-            // const response= await axios(`${API_URL}&query${searchInputValue}`); // template literals method
-            const response = await axios(API_URL, {
-                params: {
-                    query: searchInputValue,
+	//              ---------API Call--------
+	const fetchSearchList = async () => {
+		try {
+			const response = await axios(API_URL, {
+                params:{
+                    query : searchInputValue
                 }
-            });                                         // best practice
-            setSearchListValue(response.data.results);
+            });
+            setSearchListValue(response.data);
+			console.log(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-        } catch (error) {
-            console.error(error);
-        }
-    }
+	useEffect(() => {
+		const APIcall = setTimeout(() => {
+			console.log("mount");
+			fetchSearchList();
+		}, 300);
 
-    useEffect(() => {
-        const APIcall = setTimeout(() => {
-            console.log("mount")
-            fetchSearchList();
-        }, 300);
+		return () => {
+			console.log("unMount");
+			clearTimeout(APIcall);
+		};
+	}, [searchInputValue]);
 
-        return () => {
-            console.log("unMount");
-            clearTimeout(APIcall);
-        }
-    }, [searchInputValue])
+	//input-box handling
+	const handleChange = (event) => {
+		setSearchInputvalue(event.target.value);
+		const newFilteredList = searchListValue.filter((data) => {
+			return data.title.toLowerCase().includes(event.target.value.toLowerCase());
+		});
+		setSearchListValue(newFilteredList);
+	};
 
+	//clear button handling
+	const handleClearButton = () => {
+		setSearchInputvalue("");
+	};
 
+	return (
+		<div>
+			<div className="search-container">
+				<div className="title">
+					<i className="fa-solid fa-magnifying-glass fa-xl"></i>
+					<p>Looking for movies? </p>
+				</div>
 
-
-    //input-box handling
-    const handleChange = (event) => {
-        setSearchInputvalue(event.target.value);
-        const newFilteredList = searchListValue.filter((data) => {
-            return data.title.toLowerCase().includes(event.target.value.toLowerCase());
-        });
-        setSearchListValue(newFilteredList);
-    }
-
-    //clear button handling
-    const handleClearButton = () => {
-        setSearchInputvalue("");
-    }
-
-
-
-
-    return (
-        <div>
-            <div className="search-container">
-                <div className="title">
-                    <i className="fa-solid fa-magnifying-glass fa-xl"></i>
-                    <p>Looking for movies? </p>
-                </div>
-
-                <SearchInput searchInputValue={searchInputValue}
+				<SearchInput searchInputValue={searchInputValue}
                     handleChange={handleChange}
                     handleClearButton={handleClearButton}
                 />
                 <SearchList searchListValue={searchListValue} />
-            </div>
-        </div>
-    );
+			</div>
+		</div>
+	);
 };
--- server/index.js
+++ server/index.js
@@ -1,16 +1,42 @@
 console.log('=======START=======');
+const { default: axios } = require('axios');
 const express = require('express'); //express imported
-const app =express();  
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const movieList = require('./DataBase/movieData.json')
 
-const PORT =3005;
-app.listen(PORT, ()=>{ console.log(`server started in ${PORT}`);});
+console.log(movieList.length);
+//creating server
+const PORT = 3500;
+app.listen(PORT, () => { console.log(`server started in ${PORT}`); });
+app.use(cors());
+app.use(express.json());
 
-const API_URL =
-    "https://api.themoviedb.org/3/search/movie?api_key=d3449ff6ec0c027623bf6b6f5fff78b3&language=en-US&page=1&include_adult=false&query=movies";
+
+const API_URL_movieDB =
+    "https://api.themoviedb.org/3/search/movie?api_key=d3449ff6ec0c027623bf6b6f5fff78b3&language=en-US&page=1&include_adult=false&query=batman";
+
+const fetchDBmovieApi = async () => {
+    const response = axios(API_URL_movieDB);
+    const movieData = ((await response).data.results);
+    fs.writeFile(path.join(__dirname, 'DataBase', 'movieData.json'), `${JSON.stringify(movieData, null, ' ')}`, (err) => {
+        if (err) throw err;
+        console.log(`${movieData.length} movies data added to movieData.json`);
+    })
+}
+// fetchDBmovieApi();       //to fetch movie from DBmovies and store in movieData.json
+
+app.get('/', (req, res) => {
+    res.json('Hello World');
+})
+
+app.get('/api/movie', (req, res) => {
+    res.json(movieList);
+    console.log('data responded');
+})
 
 
-app.get('/', (req,res)=>{
-    res.json(API_URL);
-    
-});
 
+console.log(`========END========`);
